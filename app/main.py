db21a47254967e0ed53ea0a839535a436b5671b1
Gustavo Miranda <gustavocataldomiranda@gmail.com>
2024-09-13 22:44:43 -0300

    extract funcionando

---
+++ app/main.py
@@ -0,0 +1,5 @@
+from pipeline.extract import extract_data_from_excel
+
+lista = extract_data_from_excel('data/input')
+
+print(lista)
---
+++ app/pipeline/extract.py
@@ -0,0 +1,30 @@
+import os
+import glob # biblioteca para listar arquivos
+import pandas as pd
+
+from typing import List
+
+"""
+Função para ler os arquivos de uma pasta data/input 
+e retornar uma lista de DataFrames
+
+args: input_path (str): caminho da pasta com os arquivos
+
+return: lista de dataframes
+"""
+
+PATH = 'data/input'
+
+def extract_data_from_excel(path: str) -> List[pd.DataFrame]: # type: ignore
+    all_files = glob.glob(os.path.join(path, "*.xlsx"))
+
+    df_list = []
+    for file in all_files:
+        df_list.append(pd.read_excel(file))
+
+    return df_list
+
+
+if __name__ == "__main__":
+    df_list = extract_data_from_excel(path=PATH)
+    print(df_list)
